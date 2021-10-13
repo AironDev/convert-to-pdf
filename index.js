69bd0fb4116de32b1642246192bb4a48f2635ec7
airondev <airondev@gmail.com>
2021-10-13 22:48:22 +0100

    mode

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ module.exports.handler = async ({fileUrl, returnRaw,  Records} ) => {
   // if you want to extract a range:
   // pdftk myoldfile.pdf cat 1-2 4-5 output mynewfile.pdf
   // let fileData = 'tmp/in.pdf'
-  const numberOfPages = (execSync(`pdftk /tmp/${fileParts} dump_data | grep NumberOfPages | awk {print $2} `).toString('utf8'));
+  const numberOfPages = (execSync(`pdftk /tmp/${fileParts} dump_data | grep NumberOfPages | awk '{print $2}' `).toString('utf8'));
   console.log(numberOfPages)
   execSync(`pdftk /tmp/${fileParts} stamp /tmp/${fileParts} output /tmp/outed.pdf`, (error, stdout, stderr) => {
       if (error || stderr)
